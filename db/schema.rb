ab643614bd4ccca3b143bc998f18b3735e27418e
Diego Ávalos <gasparionni@gmail.com>
2018-09-09 05:36:27 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_09_054729) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "gyms", force: :cascade do |t|
     t.string "name"
     t.string "latitude"
